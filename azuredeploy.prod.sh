7ed6b56947d012a73b37273070bcb8830463b7fc
nickward13 <nickward@microsoft.com>
2017-11-16 16:35:16 +1100

    updated prod resource group location to West US

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -2,5 +2,5 @@
 
 az login
 
-az group create --name hectagon-shared --location "Australia Southeast"
+az group create --name hectagon-shared --location "West US"
 az group deployment create --resource-group hectagon-shared --template-file azuredeploy.json --parameters @azuredeploy.parameters.prod.json
