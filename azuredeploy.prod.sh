6b150054d1222834128e35c8ddb1b549fbf3c1ac
nickward13 <nickward@microsoft.com>
2017-11-29 12:05:45 +0800

    added tag Permanent=True

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -2,5 +2,5 @@
 
 az login
 
-az group create --name hectagon-shared --location "West US"
+az group create --name hectagon-shared --location "West US" --tags Permanent=True
 az group deployment create --resource-group hectagon-shared --template-file azuredeploy.json --parameters @azuredeploy.parameters.prod.json
